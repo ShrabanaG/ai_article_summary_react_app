3d0606a56dbb53b31dd4aae06018616c58a0e838
ShrabanaG <shrabanagoswami@gmail.com>
2024-01-28 01:04:24 +0530

    feat: remove svg file display error

---
+++ src/assets/index.ts
@@ -0,0 +1,12 @@
+// import { linkIcon } from "./link.svg";
+// import { loader } from "./loader.svg";
+// import { copy } from "./copy.svg";
+// import { logo } from "./logo.svg";
+// import { tick } from "./tick.svg";
+
+// export { linkIcon, loader, copy, logo, tick };
+export { default as Logo } from "./logo.svg";
+export { default as Loader } from "./loader.svg";
+export { default as Grid } from "./grid.svg";
+export { default as Tick } from "./tick.svg";
+export { default as LinkIcon } from "./link.svg";
